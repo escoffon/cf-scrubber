34a2e4887a3f39681c55325a33f619c92590689b
Emil Scoffone <emil@scoffone.com>
2017-05-11 21:26:43 -0700

    added a version.rb file where the gem version is now kept

---
+++ lib/cf/scrubber/version.rb
@@ -0,0 +1,11 @@
+module Cf
+  module Scrubber
+    # The version number of the cf-scrubber gem.
+
+    VERSION = '0.6.2'
+
+    # The version date for the cf-scrubber gem.
+
+    DATE = '2017-05-11'
+  end
+end
