37aa053fde5273a38f83080a18b6253a4dfeafcc
Emil Scoffone <emil@scoffone.com>
2017-09-12 22:20:50 -0700

    bumped version number and date

--- lib/cf/scrubber/version.rb
+++ lib/cf/scrubber/version.rb
@@ -2,10 +2,10 @@ module Cf
   module Scrubber
     # The version number of the cf-scrubber gem.
 
-    VERSION = '0.12.0'
+    VERSION = '0.12.1'
 
     # The version date for the cf-scrubber gem.
 
-    DATE = '2017-09-10'
+    DATE = '2017-09-12'
   end
 end
